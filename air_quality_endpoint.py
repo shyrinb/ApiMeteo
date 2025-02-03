51f80c5e7f423667bf2df25c702493e760464580
Honnok <Hujiklo.bn@gmail.com>
2025-02-03 14:38:48 +0100

    ET boum la qualité ! v2 ;-)

--- air_quality_endpoint.py
+++ air_quality_endpoint.py
@@ -11,7 +11,7 @@ def get_air_quality_openweathermap(api_key, lat, lon):
     return response.json() if response.status_code == 200 else None
 
 if __name__ == "__main__":
-    API_KEY = "votre_cle_api"
+    API_KEY = "ac2e6840732ee6b52640142d835863af"
     LOCATION = "Paris"
     LAT, LON = 48.8566, 2.3522
     
