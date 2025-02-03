4ec8d5296b8ce8da9ca68a09d903eaba827b16b1
shyrinb <100671896+shyrinb@users.noreply.github.com>
2025-02-03 15:31:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
-from flask import Flask # type: ignore
-from weather_endpoint import weather # type: ignore
-from forecast_endpoint import forecast # type: ignore
-from air_quality_endpoint import air_quality # type: ignore
-from current_weather_endpoint import current_weather # type: ignore
+from flask import Flask
+from weather_endpoint import weather
+from forecast_endpoint import forecast_bp
+from air_quality_endpoint import air_quality
+from current_weather_endpoint import current_weather
 from cities_endpoint import cities
 
 app = Flask(__name__)
 
-API_KEY = "ac2e6840732ee6b52640142d835863af"
-OPENWEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-
 # Registre des endpoints
 app.register_blueprint(weather)
-app.register_blueprint(forecast)
+app.register_blueprint(forecast_bp)
 app.register_blueprint(air_quality)
 app.register_blueprint(current_weather)
 app.register_blueprint(cities)
